6ce60f9f53dca48edba0a285d161dc4ad948a77a
khakhiD <kakkiid@gmail.com>
2023-03-27 23:01:06 +0900

    기본 시드 데이터 설정

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const EXPRESS_PORT = 3000;
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -17,12 +19,18 @@ db.once("open", () => {
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home.ejs');
 })
 
-app.listen(3000, ()=> {
-    console.log('Serving on port 3000')
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: 'My Backyard', description: 'cheap camping!'});
+    await camp.save();
+    res.send(camp);
+})
+
+app.listen(EXPRESS_PORT, ()=> {
+    console.log(`[Serving on port ${EXPRESS_PORT}]`)
 })
