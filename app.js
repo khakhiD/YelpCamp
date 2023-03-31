cc130682bfe1ff1bb2a2e629855a5b2bcabe4e8c
khakhiD <kakkiid@gmail.com>
2023-03-31 17:24:44 +0900

    Error Handling & Validation

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const { campgroundSchema } = require('./schema');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -14,19 +17,20 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
 });
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
+db.on("error", console.error.bind(console, "[DB CONNECTION ERROR!]"));
 db.once("open", () => {
-    console.log("DB CONNECTED!");
+    console.log("[DB CONNECTED!]");
 });
 
 const app = express();
 
-/* Express Settings
-    engine: ejs-mate
-    view-engine: ejs
-    views: "path/views/"
-    middleware: urlencoded, json, method-override
-*/
+/*
+ *  Express Settings
+ *  engine: ejs-mate
+ *  view-engine: ejs
+ *  views: "path/views/"
+ *   middleware: urlencoded, json, method-override
+ */
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views/'));
@@ -35,53 +39,78 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
 
+
+/**
+ * @function validateCampground
+ * Joi 모듈을 사용해서 유효성 검사를 하는 함수
+ */
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
 /* Routing */
 app.get('/', (req, res) => {
     res.render('home.ejs');
-})
+});
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res, next) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index.ejs', { campgrounds });
-})
+}));
 
 // /:id 라우트 위에 있으면 id를 new에서 찾으므로 순서가 상관이 있다.
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new.ejs');
-})
+});
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
-})
+}));
 
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     res.render('campgrounds/show.ejs', { campground });
-})
+}));
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     res.render('campgrounds/edit.ejs', { campground });
-})
+}));
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`);
-})
+}));
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id)
     res.redirect(`/campgrounds`);
+}));
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
 })
 
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if(!err.message) err.message = "꽳뺋뺧쓣쨄땴꺓! 에러났어요!";
+    res.status(statusCode).render('error.ejs', { err });
+})
 
 /* PORT OPEN, LISTENING */
 app.listen(EXPRESS_PORT, ()=> {
-    console.log(`[Serving on port ${EXPRESS_PORT}]`)
+    console.log(`[SERVING ON PORT ${EXPRESS_PORT}]`)
 })
