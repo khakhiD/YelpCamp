7bf1585a43466c6cb3211614928fb0c859a52dbb
khakhiD <kakkiid@gmail.com>
2023-04-04 23:53:10 +0900

    static assets

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.set('views', path.join(__dirname, '/views/'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/reviews', reviewRoutes);
