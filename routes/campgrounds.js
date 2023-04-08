3cf07ce83b37d6f3f765eba8a0970f962b326b6f
khakhiD <kakkiid@gmail.com>
2023-04-08 23:34:36 +0900

    feat: image upload with multer

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,11 +3,17 @@ const router = express.Router();
 const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/ '});
 
 // Routes
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files);
+        res.send("확인해봐");
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
