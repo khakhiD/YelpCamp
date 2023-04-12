7b7bc81ad98b6ec83be22a90dadab78c5a4d306d
khakhiD <kakkiid@gmail.com>
2023-04-12 21:47:57 +0900

    debugging

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mbxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mbxToken });
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 
@@ -24,12 +28,20 @@ const seedDB = async () => {
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
+        const randomCity = `${cities[random1000].city}, ${cities[random1000].state}`
+        const geoData = await geocoder.forwardGeocode({
+            query: randomCity,
+            limit: 1
+        }).send();
+        const geometry = geoData.body.features[0].geometry;
         const camp = new Campground({
-            author: '642ec329db172593ac565ac0',
+            // YOUR USER ID
+            author: '642eb4d28c7980988c14d616',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nobis incidunt fugiat asperiores, odio doloremque. Dicta laboriosam aspernatur dolorum qui vitae, doloribus suscipit iste ipsum placeat quos consequuntur cum inventore!',
             price,
+            geometry,
             images: [
                 {
                     url: 'https://res.cloudinary.com/dmhrfbfol/image/upload/v1681043908/YelpCamp/kevin-ianeselli-ebnlHkqfUHY-unsplash_mrgcdj.jpg',
