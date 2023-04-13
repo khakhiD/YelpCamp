032cc3bdfe00ba93a0ec0a59a4ef190ec1e0b24b
khakhiD <kakkiid@gmail.com>
2023-04-13 20:48:21 +0900

    feat: cluster map for index page

--- models/campground.js
+++ models/campground.js
@@ -9,7 +9,9 @@ const ImageSchema = new Schema({
 
 ImageSchema.virtual('thumbnail').get(function() {
   return this.url.replace("/upload", "/upload/w_200");
-})
+});
+
+const opts = { toJSON: { virtuals: true }};
 
 const campgroundSchema = new Schema({
   title: String,
@@ -38,6 +40,12 @@ const campgroundSchema = new Schema({
       ref: 'Review',
     },
   ]
+}, opts);
+
+campgroundSchema.virtual('properties.popUpMarkup').get(function() {
+  return `
+  <strong><a href=/campgrounds/${this._id}>${this.title}</a></strong>
+  <p>${this.description.substring(0, 20)}...</p>`
 });
 
 campgroundSchema.post('findOneAndDelete', async(doc) => {
--- seeds/index.js
+++ seeds/index.js
@@ -25,15 +25,15 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     // const c = new Campground({ title: 'purple field' });
     // await c.save();
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 300; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
-        const randomCity = `${cities[random1000].city}, ${cities[random1000].state}`
-        const geoData = await geocoder.forwardGeocode({
-            query: randomCity,
-            limit: 1
-        }).send();
-        const geometry = geoData.body.features[0].geometry;
+        // const randomCity = `${cities[random1000].city}, ${cities[random1000].state}`
+        // const geoData = await geocoder.forwardGeocode({
+        //     query: randomCity,
+        //     limit: 1
+        // }).send();
+        // const geometry = geoData.body.features[0].geometry;
         const camp = new Campground({
             // YOUR USER ID
             author: '642eb4d28c7980988c14d616',
@@ -41,7 +41,13 @@ const seedDB = async () => {
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nobis incidunt fugiat asperiores, odio doloremque. Dicta laboriosam aspernatur dolorum qui vitae, doloribus suscipit iste ipsum placeat quos consequuntur cum inventore!',
             price,
-            geometry,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
+            },
             images: [
                 {
                     url: 'https://res.cloudinary.com/dmhrfbfol/image/upload/v1681043908/YelpCamp/kevin-ianeselli-ebnlHkqfUHY-unsplash_mrgcdj.jpg',
